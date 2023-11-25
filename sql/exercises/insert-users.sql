9258e63ccf71a51f4cc40e57c7d4b8bde8489f0c
danielrosenosi <devroseno@gmail.com>
2023-11-25 14:19:20 -0300

    feat: exercise "insert five users"

---
+++ sql/exercises/insert-users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (full_name, email, password_hash) VALUES
+('Carlos Daniel Roseno de Lima', 'carlosdaniel@gmail.com', 'xmnq31jfd13j'),
+('José Eduardo Ferreira', 'eduardoferreira@gmail.com', 'ca8hashca8saa'),
+('Ana Carolina Pereira Nascimento', 'anacarolina@gmail.com', 'xmdasnq31jfd13j'),
+('Pedro Lima', 'pedrolima@gmail.com', 'xmnq3dasddas1jfd13j'),
+('Carlos Gabriel', 'carlosgabriel@gmail.com', 'xmnq31jfdcsasas13j');
